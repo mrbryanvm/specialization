936b7467fddd2e1df62bfae0a14b6d1af9ec12f1
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-02-14 22:40:46 -0400

    decision.py | programa para ver si puedes entrar o no al bar

---
+++ FASE_1_ARRANQUE/Mes_01_Feb_Mar/Week_01/Day_01/decision.py
@@ -0,0 +1,8 @@
+edad_str = input("Ingresa tu edad:")
+edad = int(edad_str)
+if edad >= 18:
+    print("Bienvenido al bar")
+    print ("Pedite una cerveza choco")
+else:
+    print("Andate a estudiar, eres menor")
+print("Fin del programa")
