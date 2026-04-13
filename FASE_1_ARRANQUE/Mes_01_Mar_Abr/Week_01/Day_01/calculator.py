9c916b8a35bdc8894d749813d6d76ec2a742ae67
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-04-13 13:34:59 -0400

    feat: add interactive menu with user input

--- FASE_1_ARRANQUE/Mes_01_Mar_Abr/Week_01/Day_01/calculator.py
+++ FASE_1_ARRANQUE/Mes_01_Mar_Abr/Week_01/Day_01/calculator.py
@@ -1,19 +1,42 @@
 # Calculadora Interactiva - Día 1
-def sumar(a, b):
+def add(a, b):
     return a + b
 
-def restar(a, b):
+def subtract(a, b):
     return a - b
 
-def multiplicar(a, b):
+def multiply(a, b):
     return a * b
 
-def dividir(a, b):
+def divide(a, b):
     return a / b
 
-def calculadora():
+def calculator():
     print("INTERACTIVE CALCULATOR")
     print("1. Add")
-    print("2. Substract")
-    print ("3. Multiply")
+    print("2. Subtract")
+    print("3. Multiply")
     print("4. Divide")
+
+    option = input("Choose an option (1-4); ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if option == "1":
+        result = add(num1, num2)
+        print(f"Result: {result}")
+    elif option == "2":
+        result = subtract(num1, num2)
+        print(f"Result: {result}")
+    elif option == "3":
+        result = multiply(num1, num2)
+        print(f"Result: {result}")
+    elif option == "4":
+        result = divide(num1, num2)
+        print(f"Result: {result}")
+    else:
+        print("invalid option")
+
+if __name__ == "__main__":
+    calculator()
