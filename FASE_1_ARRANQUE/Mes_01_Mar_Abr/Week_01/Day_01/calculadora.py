02aae015e16578144dfa0014c23ed18e7b7e727c
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-04-13 11:51:23 -0400

    feat: add basic arithmetic functions

---
+++ FASE_1_ARRANQUE/Mes_01_Mar_Abr/Week_01/Day_01/calculadora.py
@@ -0,0 +1,12 @@
+# Calculadora Interactiva - Día 1
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    return a / b
