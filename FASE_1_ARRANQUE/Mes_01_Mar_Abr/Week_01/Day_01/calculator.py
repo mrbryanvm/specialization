ceaaf7fe493213d16763dca2864ca5c6de8fe2c2
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-04-13 14:35:03 -0400

    feat(calculator): add loop and exit option

--- FASE_1_ARRANQUE/Mes_01_Mar_Abr/Week_01/Day_01/calculator.py
+++ FASE_1_ARRANQUE/Mes_01_Mar_Abr/Week_01/Day_01/calculator.py
@@ -12,31 +12,38 @@ def divide(a, b):
     return a / b
 
 def calculator():
-    print("INTERACTIVE CALCULATOR")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
+    while(True):
+        print("INTERACTIVE CALCULATOR")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
 
-    option = input("Choose an option (1-4); ")
+        option = input("Choose an option (1-5); ")
+        if option == "5":
+            print("Goodbye")
+            break
 
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+        if option not in ["1", "2", "3", "4"]:
+            print("Invalid option, try again.")
+            continue
+      
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-    if option == "1":
-        result = add(num1, num2)
-        print(f"Result: {result}")
-    elif option == "2":
-        result = subtract(num1, num2)
-        print(f"Result: {result}")
-    elif option == "3":
-        result = multiply(num1, num2)
-        print(f"Result: {result}")
-    elif option == "4":
-        result = divide(num1, num2)
-        print(f"Result: {result}")
-    else:
-        print("invalid option")
-
+        if option == "1":
+            result = add(num1, num2)
+            print(f"Result: {result}")
+        elif option == "2":
+            result = subtract(num1, num2)
+            print(f"Result: {result}")
+        elif option == "3":
+            result = multiply(num1, num2)
+            print(f"Result: {result}")
+        elif option == "4":
+            result = divide(num1, num2)
+            print(f"Result: {result}")
+            
 if __name__ == "__main__":
     calculator()
