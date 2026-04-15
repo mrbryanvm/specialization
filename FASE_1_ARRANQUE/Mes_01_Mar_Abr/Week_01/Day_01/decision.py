acd6c923d2cee0ee558d9bd11f5e3a640aa04241
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-04-15 17:45:05 -0400

    refactor: remove old Month 01 March-April files

--- FASE_1_ARRANQUE/Mes_01_Mar_Abr/Week_01/Day_01/decision.py
+++
@@ -1,8 +0,0 @@
-edad_str = input("Ingresa tu edad:")
-edad = int(edad_str)
-if edad >= 18:
-    print("Bienvenido al bar")
-    print ("Pedite una cerveza choco")
-else:
-    print("Andate a estudiar, eres menor")
-print("Fin del programa")
