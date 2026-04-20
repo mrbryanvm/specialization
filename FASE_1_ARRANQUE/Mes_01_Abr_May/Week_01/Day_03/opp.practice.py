19365b024b1d11afb5f80550822f86a43e923c05
mr_bryanvm <bryan.vasquez.456@gmail.com>
2026-04-20 10:18:15 -0400

    feat: modify if __name__ == main in opp file

--- FASE_1_ARRANQUE/Mes_01_Abr_May/Week_01/Day_03/opp.practice.py
+++ FASE_1_ARRANQUE/Mes_01_Abr_May/Week_01/Day_03/opp.practice.py
@@ -25,7 +25,25 @@ class Product:
         print(f"Product: {self.name} | Price: ${self.price} | Stock: {self.stock}")
 
 
+class Product2:
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock
+    
+    def sell(self, quantity):
+        if quantity > 0 and self.stock >= quantity:
+            self.stock -= quantity
+            print(f"Success: Sold {quantity} units of {self.name}")
+        else:
+            print(f"Error: Invalid quantity or not enough stock for {self.name}")
+    
+    def display_info(self):
+        print(f"Product: {self.name} |  Price: {self.price}  |  Stock: {self.stock}")
+
+
 # Testing the class
+print(f"==========================TESTING PRODUCT CLASS==========================")
 if __name__ == "__main__":
     # Instantiate the object (No 'new' keyword needed)
     laptop = Product("MacBook Pro", 1500, 10)
@@ -40,4 +58,12 @@ if __name__ == "__main__":
     laptop.display_info()
     
     # Test error case
-    laptop.sell(15)
+    laptop.sell(15) 
+
+    # Testing the class Product2
+    print(f"==========================TESTING PRODUCT 2 CLASS==========================")
+    laptop2 = Product2("Dell Inspiron", 800, 15)
+    laptop2.display_info()
+    laptop2.sell(5)
+    laptop2.display_info()
+    laptop2.sell(100)
